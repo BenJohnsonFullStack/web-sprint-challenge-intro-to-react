113d51b5e4b7864a82bc8704427a3f40830ce655
Ben Johnson <monk.sound.design@gmail.com>
2023-05-31 14:17:50 -0400

    api request, setData

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+
+`
+
+
+
+export default function Characters() {
+    return (
+
+    )
+}
